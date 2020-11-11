a010dc85fd5a3b3954383d168d69950e5ad47b43
Emilio Kartono <emiliokartono@gmail.com>
2020-11-10 23:47:19 -0500

    fix: refactored files

--- Mac Catalyst App/Mac Catalyst App/App/AppDelegate.swift
+++ Mac Catalyst App/Mac Catalyst App/App/AppDelegate.swift
@@ -14,26 +14,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var mqttClient = MQTTClient("192.168.0.102", 8888, "client", "username", "password")
 
+    // Override point for customization after application launch.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        
     }
-
-
 }
 
