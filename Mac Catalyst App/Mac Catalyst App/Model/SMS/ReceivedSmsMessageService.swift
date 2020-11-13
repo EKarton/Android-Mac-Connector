faed29ac1b01e8178a93b048a74a207d9602a79a
Emilio Kartono <emiliokartono@gmail.com>
2020-11-13 00:45:47 -0500

    feat: mac app shows local notifications when a new msg is received from a device

---
+++ Mac Catalyst App/Mac Catalyst App/Model/SMS/ReceivedSmsMessageService.swift
@@ -0,0 +1,9 @@
+//
+//  ReceivedSmsMessageService.swift
+//  Mac Catalyst App
+//
+//  Created by Emilio Kartono on 2020-11-13.
+//  Copyright © 2020 Emilio Kartono. All rights reserved.
+//
+
+import Foundation
